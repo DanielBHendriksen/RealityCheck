c1486f40f0607f071a58d8b343fc817c8319f81b
DanielBHendriksen <danielbhendriksen93@gmail.com>
2025-06-15 12:05:30 +0200

    Add RealityCheck iOS SwiftUI app and update .gitignore

---
+++ RealityCheck.IOS/RealityCheck/RealityCheck/RealityCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealityCheckApp.swift
+//  RealityCheck
+//
+//  Created by Daniel Hendriksen on 15/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct RealityCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
